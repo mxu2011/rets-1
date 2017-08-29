36b428f530818149861481e391f39517dbb92450
Martin Xu <mxu2008@gmail.com>
2017-08-28 23:21:38 -0700

    tried with treb-idx

--- example/connect.rb
+++ example/connect.rb
@@ -2,9 +2,9 @@ require 'rets'
 
 # Pass the :login_url, :username, :password and :version of RETS
 client = Rets::Client.new({
-  login_url: 'login_url',
-  username: 'xxx',
-  password: 'xxx',
+  login_url: 'http://rets.torontomls.net:6103/rets-treb3pv/server/login',
+  username: 'D16nic',
+  password: 'B#65n17',
   version: 'RETS/1.5'
 })
 
--- example/get-photos.rb
+++ example/get-photos.rb
@@ -1,13 +1,22 @@
 require 'rets'
 
 
+# Pass the :login_url, :username, :password and :version of RETS
+client = Rets::Client.new({
+                              login_url: 'http://rets.torontomls.net:6103/rets-treb3pv/server/login',
+                              username: 'D16nic',
+                              password: 'B#65n17',
+                              version: 'RETS/1.5'
+                          })
+
+
 # Get all photos (*) for MLS ID 'mls_id'
 # Pass :object_id (ie '0', '1,2', wildcard '*')
 # The pass :resource (Property, Agent, MetaData, ...), :object_type (Photo, PhotoLarge), :rescource_id (ID of agent, MLS, ...)
 photos = client.objects '*', {
   resource: 'Property',
   object_type: 'Photo',
-  resource_id: 'mls_id'
+  resource_id: 'W3906742'
 }
 
 photos.each_with_index do |data, index|
