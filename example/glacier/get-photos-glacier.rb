510996a7546a1a87e6a2e71116c7c4f1f105a142
Martin Xu <mxu2008@gmail.com>
2017-09-07 07:38:21 -0700

    added glacier which is actually BRC with different credential

--- example/glacier/get-photos-glacier.rb
+++ example/glacier/get-photos-glacier.rb
@@ -1,12 +1,14 @@
 require 'rets'
 require 'json'
+require '../common'
 
+# glacier is actually BRC with different credential
 
 # Pass the :login_url, :username, :password and :version of RETS
 client = Rets::Client.new({
-                              login_url: 'http://rewwls:<RETS_WLS_PASSWORD>@matrix.buywhistler.com/rets/login.ashx',
-                              username: 'rewwls',
-                              password: 'powder',
+                              login_url: 'http://reb.retsiq.com/contactres/rets/login',
+                              username: 'RETSREWDOTCAGVIC',
+                              password: '93X63IYUQ',
                               version: 'RETS/1.7.2'
                           })
 
@@ -16,32 +18,10 @@ client = Rets::Client.new({
 # The pass :resource (Property, Agent, MetaData, ...), :object_type (Photo, PhotoLarge), :rescource_id (ID of agent, MLS, ...)
 photos = client.objects '*', {
   resource: 'Property',
-  object_type: 'LargePhoto',
-  resource_id: '18272734' # for wls, this is mls_num
+  object_type: 'Photo',
+  resource_id: '262218062' # for glacier, this is mls_num
 }
 
-photos.each_with_index do |data, index|
-  seq = data.headers['object-id'] if data.respond_to? ('headers')
+Common::process_photos(photos)
 
-  puts "object-id=#{seq}, index=#{index}"
-
-  seq = (seq.to_i - 1) if !seq.nil?
-
-  seq ||= index
-
-  # write jpg files
-  File.open("image-#{seq.to_s}.jpg", 'w') do |file|
-
-    file.write data.body
-  end
-
-  # serialize each image data object to a json text file
-  File.open("image-#{seq.to_s}.json", 'w') do |file|
-    data.body = nil
-    file.write(JSON.pretty_generate(data.to_h))
-  end
-
-end
-
-puts photos.length.to_s + ' photos saved.'
 client.logout
